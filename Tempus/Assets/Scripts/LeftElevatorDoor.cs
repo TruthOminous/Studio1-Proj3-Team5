ddc0e5e7b02ea0f04fb0ac0c2689a84d926b29d7
Levi Stenton <1005973@student.SAE.edu.au>
2016-10-22 21:58:28 +1000

    Functional elevator door and skybox
    The elevator door the player starts in is now functional as well as the
    skybox is now in place. Lighting is now in the elevators and another
    light is rasied over three different coloured cubes that appearr grey in
    game but in the inspector 3 different colours.

---
+++ Tempus/Assets/Scripts/LeftElevatorDoor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftElevatorDoor : MonoBehaviour {
+
+    Transform myTransform;
+
+    PlayerControls playerControls;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myTransform = this.transform;
+
+        playerControls = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControls>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z <= 241.85f)
+        {
+            myTransform.position += new Vector3(0, 0, 0.01f);
+        }
+
+        if (transform.position.z >= 240.03f)
+        {
+            if (playerControls.transform.position.x <= 242f)
+            {
+                myTransform.position += new Vector3(0, 0, -0.02f);
+            }
+        }
+    }
+}
---
+++ Tempus/Assets/Scripts/RightElevatorDoor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightElevatorDoor : MonoBehaviour {
+
+    Transform myTransform;
+
+    PlayerControls playerControls;
+
+    // Use this for initialization
+    void Start ()
+    {
+        myTransform = this.transform;
+
+        playerControls = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControls>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z >= 236.15f)
+        {
+            myTransform.position += new Vector3(0, 0, -0.01f);
+        }
+
+        if(transform.position.z <= 237.99f)
+        {
+            if (playerControls.transform.position.x <= 242f)
+            {
+                myTransform.position += new Vector3(0, 0, 0.02f);
+            }
+        }
+    }
+}
