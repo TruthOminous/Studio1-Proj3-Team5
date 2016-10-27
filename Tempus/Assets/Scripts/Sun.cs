843b1c0b628f39eaa30209f68470280ef4966ae7
Levi Stenton <1005973@student.SAE.edu.au>
2016-10-28 05:23:41 +1000

    I made things pretty now!
    Things are pretty and Dakota fixed the sliding.

---
+++ Tempus/Assets/Scripts/Sun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sun : MonoBehaviour {
+
+    Transform myTransform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myTransform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myTransform.position += new Vector3(0, -0.16f, 0);
+    }
+}
